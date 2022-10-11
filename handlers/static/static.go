93217e2c8b63f4e35f81a085ec04a56f4c3b6396
josedev <joseijd69@gmail.com>
2022-10-11 14:24:33 -0500

    fix load static js files

--- handlers/static/static.go
+++ handlers/static/static.go
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2016-2017 dapperdox.com 
+Copyright (C) 2016-2017 dapperdox.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -13,7 +13,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 */
 package static
 
@@ -54,7 +53,7 @@ func Register(r *pat.Router) {
 		switch {
 		case strings.HasPrefix(mimeType, "image"),
 			strings.HasPrefix(mimeType, "text/css"),
-			strings.HasSuffix(mimeType, "javascript"):
+			strings.Contains(mimeType, "javascript"):
 			allow = true
 		default:
 			allow = false
